605c7d64929590db8f0db2c27a6f1ac752f3e6c1
Harun Dastekin <hdastekin@gmail.com>
2025-08-07 19:23:22 +0200

    Initial commit

---
+++ src/main/java/com/zelkulon/entwurfsmuster/MyEntwurfsmusterApplication.java
@@ -0,0 +1,13 @@
+package com.zelkulon.entwurfsmuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEntwurfsmusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEntwurfsmusterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zelkulon/entwurfsmuster/MyEntwurfsmusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelkulon.entwurfsmuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEntwurfsmusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
